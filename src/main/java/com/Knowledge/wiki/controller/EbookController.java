8034dd4412db903db3458e271e139d3b23fd7fec
goner <1577227473@qq.com>
2022-07-05 16:18:14 +0800

    增加通用的返回类CommonResp，以后所有后端返回值全都是该类型

--- src/main/java/com/Knowledge/wiki/controller/EbookController.java
+++ src/main/java/com/Knowledge/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.Knowledge.wiki.controller;
 
 import com.Knowledge.wiki.domain.Ebook;
+import com.Knowledge.wiki.resp.CommonResp;
 import com.Knowledge.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,11 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
