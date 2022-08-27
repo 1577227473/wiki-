ca36ba40197f5ac249cb0d167b11739f92c0a12b
goner <1577227473@qq.com>
2022-08-27 15:40:06 +0800

    增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器、拦截器

--- src/main/java/com/Knowledge/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/Knowledge/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,20 @@
-package com.Knowledge.wiki.config;
-
-import com.Knowledge.wiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.Knowledge.wiki.config;
+//
+//import com.Knowledge.wiki.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
