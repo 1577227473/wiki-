2836a9301d8d36c377c4fa697484580690e32555
goner <1577227473@qq.com>
2022-08-30 20:58:05 +0800

    分类表格按树形结构表示 增加数组转树形数据工具的方法

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -21,7 +21,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -104,6 +104,19 @@ export default defineComponent({
       },
     ];
 
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1=ref();//一级分类，children属性就是二级分类
+
     /*
      *数据查询
      **/
@@ -114,7 +127,11 @@ export default defineComponent({
         const data=response.data;
         if(data.success){
           categorys.value = data.content;
+          console.log("原始数据",categorys.value);
 
+          level1.value=[];
+          level1.value = Tool.array2Tree(categorys.value,0);
+          console.log("树形结构",level1);
         } else {
           message.error(data.message);
         }
@@ -172,7 +189,8 @@ export default defineComponent({
 
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
