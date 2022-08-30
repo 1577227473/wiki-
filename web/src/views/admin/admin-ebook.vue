e85da683873a6098109efb20b7fd336721f0faeb
goner <1577227473@qq.com>
2022-08-30 23:09:36 +0800

    分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -67,11 +67,12 @@
         <a-form-item label="名称">
           <a-input v-model:value="ebook.name" />
         </a-form-item>
-        <a-form-item label="分类一">
-          <a-input v-model:value="ebook.category1Id" />
-        </a-form-item>
-        <a-form-item label="分类二">
-          <a-input v-model:value="ebook.category2Id" />
+        <a-form-item label="分类">
+          <a-cascader
+            v-model:value = "categoryIds"
+            :field-names = "{label:'name',value:'id',children:'children'}"
+            :options = "level1"
+          />
         </a-form-item>
         <a-form-item label="描述">
           <a-input v-model:value="ebook.description" type="textarea"/>
@@ -177,11 +178,17 @@ export default defineComponent({
 
 
     //---------表单----------
-    const ebook = ref({});
+    /*
+     数组 [100,101] 对应: 前端开发/vue
+     */
+    const categoryIds = ref();
+    const ebook = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/ebook/save",ebook.value).then((response)=>{
         modalLoading.value = false;
         const data=response.data;//data = CommonResp
@@ -205,6 +212,7 @@ export default defineComponent({
     const edit = (record:any) => {
       modalVisible.value=true;
       ebook.value=Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id,ebook.value.category2Id]
     };
 
     /*
@@ -212,7 +220,8 @@ export default defineComponent({
     */
     const add = () => {
       modalVisible.value=true;
-      ebook.value= {};    };
+      ebook.value= {};
+    };
 
     const handleDelete = (id: number) => {
       axios.delete("/ebook/delete/"+id).then((response)=>{
@@ -227,7 +236,30 @@ export default defineComponent({
       });
     };
 
+    const level1 = ref();
+    /**
+     * 查询所有分类
+     */
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response)=>{
+        loading.value=false;
+        const data = response.data;
+        if(data.success){
+          const categorys = data.content;
+          console.log("原始数组:",categorys);
+
+          level1.value=[];
+          level1.value=Tool.array2Tree(categorys,0);
+          console.log("树形结构",level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     onMounted(()=>{
+      handleQueryCategory();
       handleQuery({
         page:1,
         size:pagination.value.pageSize
@@ -250,6 +282,8 @@ export default defineComponent({
       modalVisible,
       modalLoading,
       handleModalOk,
+      categoryIds,
+      level1,
 
       handleDelete
     }
