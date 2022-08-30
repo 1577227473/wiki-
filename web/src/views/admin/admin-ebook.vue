bd6c79449471cefa93e1aaf05e249c769a762649
goner <1577227473@qq.com>
2022-08-30 23:36:18 +0800

    电子书列表显示分类名称

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -33,6 +33,9 @@
         <template #cover="{ text:cover }">
           <img v-if="cover" :src="cover" alt="avatar"/>
         </template>
+        <template v-slot:category="{text,record}">
+          <span>{{getCategoryName(record.category1Id)}} / {{getCategoryName(record.category2Id)}}</span>
+        </template>
         <template v-slot:action="{ text,record}">
           <a-space size="small">
             <a-button type="primary" @click="edit(record)">
@@ -112,13 +115,8 @@ export default defineComponent({
         dataIndex: 'name',
       },
       {
-        title: '分类一',
-        key:'category1Id',
-        dataIndex: 'category1Id',
-      },
-      {
-        title: '分类二',
-        dataIndex: 'category2Id',
+        title: '分类',
+        slots: {customRender: 'category'}
       },
       {
         title: '文档数',
@@ -237,6 +235,7 @@ export default defineComponent({
     };
 
     const level1 = ref();
+    let categorys:any;
     /**
      * 查询所有分类
      */
@@ -246,7 +245,7 @@ export default defineComponent({
         loading.value=false;
         const data = response.data;
         if(data.success){
-          const categorys = data.content;
+          categorys = data.content;
           console.log("原始数组:",categorys);
 
           level1.value=[];
@@ -256,6 +255,16 @@ export default defineComponent({
           message.error(data.message);
         }
       });
+    };
+
+    const getCategoryName = (cid:number) => {
+      let result = "";
+      categorys.forEach((item:any)=>{
+        if(item.id === cid){
+          result = item.name;
+        }
+      });
+      return result;
     };
 
     onMounted(()=>{
@@ -274,6 +283,7 @@ export default defineComponent({
       loading,
       handleTableChange,
       handleQuery,
+      getCategoryName,
 
       edit,
       add,
