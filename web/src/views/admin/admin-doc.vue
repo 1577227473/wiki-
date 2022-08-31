17d306d4cb0667786749b4cf08ed9cb4b01f1b53
goner <1577227473@qq.com>
2022-08-31 14:34:05 +0800

    使用递归算法，将树形数据中，当前目标节点及其子孙节点设计成disabled。 解决编辑保存后，重新编辑时，数据不更新问题。 为树形选择增加一个“无”选择，用来新增一级节点

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -60,6 +60,18 @@
           <a-input v-model:value="doc.name" />
         </a-form-item>
         <a-form-item label="父文档">
+          <a-tree-select
+              v-model:value="doc.parent"
+              style="width: 100%"
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+              :tree-data="treeSelectData"
+              placeholder="选择父文档"
+              tree-default-expand-all
+              :replaceFields="{title:'name',key:'id',value:'id'}"
+          >
+          </a-tree-select>
+        </a-form-item>
+        <a-form-item label="父文档">
           <a-select
               ref="select"
               v-model:value="doc.parent"
@@ -130,6 +142,8 @@ export default defineComponent({
      **/
     const handleQuery = () => {
       loading.value=true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      level1.value = [];
       axios.get("/doc/all").then((response)=>{
         loading.value=false;
         const data=response.data;
@@ -147,6 +161,9 @@ export default defineComponent({
     };
 
     //---------表单----------
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value=[];
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
@@ -166,12 +183,43 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData:any,id:any) => {
+      //遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node=treeSelectData[i];
+        if(node.id===id){
+          //如果当前节点就是目标节点
+          console.log("disable",node);
+          //将目标节点设计为disable
+          node.disabled=true;
+
+          //遍历所有子节点，将所有子节点全部都加上disable
+          const children=node.children;
+          if(Tool.isNotEmpty(children)){
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children,children[j].id);
+            }
+          }
+        }
+      }
+    }
+
     /*
     编辑
      */
     const edit = (record:any) => {
       modalVisible.value=true;
       doc.value=Tool.copy(record);
+
+      //不能选择当前节点及所有子节点作为父节点，不然会使树断开
+      treeSelectData.value=Tool.copy(level1.value);
+      setDisable(treeSelectData.value,record.id);
+
+      //为选择数添加一个“无”
+      treeSelectData.value.unshift({id:0,name:'无'});
     };
 
     /*
@@ -179,7 +227,13 @@ export default defineComponent({
     */
     const add = () => {
       modalVisible.value=true;
-      doc.value= {};    };
+      doc.value= {};
+
+      treeSelectData.value=Tool.copy(level1.value);
+
+      //为选择树添加一个“无”
+      treeSelectData.value.unshift({id:0,name:'无'});
+    };
 
     const handleDelete = (id: number) => {
       axios.delete("/doc/delete/"+id).then((response)=>{
@@ -211,7 +265,8 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
 
-      handleDelete
+      handleDelete,
+      treeSelectData
     }
   }
 });
