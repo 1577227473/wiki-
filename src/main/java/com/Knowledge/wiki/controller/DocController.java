484526ace886f18f6097f48ecaf369e9bc368956
goner <1577227473@qq.com>
2022-08-31 13:39:39 +0800

    从分类管理复制出文档管理的前后端代码 遗留问题：编辑表单中，只能选择一级文档作为父文档

---
+++ src/main/java/com/Knowledge/wiki/controller/DocController.java
@@ -0,0 +1,52 @@
+package com.Knowledge.wiki.controller;
+
+import com.Knowledge.wiki.req.DocQueryReq;
+import com.Knowledge.wiki.req.DocSaveReq;
+import com.Knowledge.wiki.resp.DocQueryResp;
+import com.Knowledge.wiki.resp.CommonResp;
+import com.Knowledge.wiki.resp.PageResp;
+import com.Knowledge.wiki.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq docQueryReq) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(docQueryReq);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq docReq) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(docReq);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+
+}
---
+++ src/main/java/com/Knowledge/wiki/service/DocService.java
@@ -0,0 +1,92 @@
+package com.Knowledge.wiki.service;
+
+
+import com.Knowledge.wiki.domain.Doc;
+import com.Knowledge.wiki.domain.DocExample;
+import com.Knowledge.wiki.mapper.DocMapper;
+import com.Knowledge.wiki.req.DocQueryReq;
+import com.Knowledge.wiki.req.DocSaveReq;
+import com.Knowledge.wiki.resp.DocQueryResp;
+import com.Knowledge.wiki.resp.PageResp;
+import com.Knowledge.wiki.util.CopyUtil;
+import com.Knowledge.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<DocQueryResp> list(DocQueryReq docQueryReq)
+    {
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+        PageHelper.startPage(docQueryReq.getPage(), docQueryReq.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo=new PageInfo<>(docList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+
+//        List<DocResp> respList = new ArrayList<>();
+//        for (Doc doc : docList) {
+//            //DocResp docResp = new DocResp();
+//            //BeanUtils.copyProperties(doc,docResp);
+//            //对象复制
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//        }
+
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+    public void save(DocSaveReq req){
+        Doc doc=CopyUtil.copy(req,Doc.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            //Id为空，则新增
+            doc.setId(snowFlake.nextId());
+            docMapper.insert(doc);
+        } else {
+            //Id不为空，则更新
+            docMapper.updateByPrimaryKey(doc);
+        }
+    }
+
+    public void delete(Long id) {
+        docMapper.deleteByPrimaryKey(id);
+    }
+
+    public List<DocQueryResp> all()
+    {
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return list;
+    }
+}
