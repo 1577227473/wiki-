32657dc6ecab0e9cc82cf128cbaf24c1f4cf9678
goner <1577227473@qq.com>
2022-09-02 20:48:50 +0800

    增加保存文档成功提示 编辑和新增的时候，先清空富文本

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -192,8 +192,8 @@ export default defineComponent({
         modalLoading.value = false;
         const data=response.data;//data = CommonResp
         if(data.success){
-          modalVisible.value = false;
-
+          // modalVisible.value = false;
+          message.success("保存成功!");
           //重新加载列表
           handleQuery();
         } else {
@@ -282,6 +282,8 @@ export default defineComponent({
     编辑
      */
     const edit = (record:any) => {
+      //清空文档
+      editor.txt.html("");
       modalVisible.value=true;
       doc.value=Tool.copy(record);
       handleQueryContent();
@@ -298,6 +300,8 @@ export default defineComponent({
     新增
     */
     const add = () => {
+      //清空文档
+      editor.txt.html("");
       modalVisible.value=true;
       doc.value= {
         ebookId:route.query.ebookId
