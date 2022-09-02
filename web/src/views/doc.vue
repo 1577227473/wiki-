491927699ea1afc8702bc61b1f0d89ec20b88ad0
goner <1577227473@qq.com>
2022-09-02 22:10:45 +0800

    打开文档页面时，默认选中第一个节点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,10 +9,12 @@
             @select="onSelect"
             :replaceFields="{title:'name',key:'id',value:'id'}"
             :defaultExpandAll="true"
+            :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -33,6 +35,9 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value=[];
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -48,6 +53,21 @@ export default defineComponent({
     const level1=ref();//一级文档，children属性就是二级文档
     level1.value=[];
 
+
+    /*
+     *内容查询
+     **/
+    const handleQueryContent = (id:number) => {
+      axios.get("/doc/find-content/"+id).then((response)=>{
+        const data=response.data;
+        if(data.success){
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     /*
      *数据查询
      **/
@@ -61,6 +81,11 @@ export default defineComponent({
 
           level1.value=[];
           level1.value = Tool.array2Tree(docs.value,0);
+
+          if(Tool.isNotEmpty(level1)){
+            defaultSelectedKeys.value=[level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+          }
         } else {
           message.error(data.message);
         }
@@ -68,12 +93,23 @@ export default defineComponent({
     };
 
 
+    const onSelect = (selectedKeys:any,info:any) => {
+      console.log('selected',selectedKeys,info);
+      if (Tool.isNotEmpty(selectedKeys)){
+        //加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
+    }
+
     onMounted(()=>{
       handleQuery();
     });
 
     return {
       level1,
+      html,
+      onSelect,
+      defaultSelectedKeys
     }
   }
 });
