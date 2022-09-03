e161aab2d18b208528a38aadc5fbe4c633685b6d
goner <1577227473@qq.com>
2022-09-03 20:26:05 +0800

    修改的时候，用户名不允许修改

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -59,7 +59,7 @@
     >
       <a-form :model="user" :label-col="{span:6}" :wrapper-col="{ span: 18 }">
         <a-form-item label="登录名">
-          <a-input v-model:value="user.loginName" />
+          <a-input v-model:value="user.loginName" :disabled="!!user.id"/>
         </a-form-item>
         <a-form-item label="昵称">
           <a-input v-model:value="user.name" />
