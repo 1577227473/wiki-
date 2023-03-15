9d578fffae36082d44284aef64e34e670670883f
goner <1577227473@qq.com>
2023-03-15 21:01:31 +0800

    集成redis,登录后将登录信息放入redis中

--- src/main/java/com/Knowledge/wiki/controller/UserController.java
+++ src/main/java/com/Knowledge/wiki/controller/UserController.java
@@ -9,18 +9,33 @@ import com.Knowledge.wiki.resp.PageResp;
 import com.Knowledge.wiki.resp.UserLoginResp;
 import com.Knowledge.wiki.resp.UserQueryResp;
 import com.Knowledge.wiki.service.UserService;
+import com.Knowledge.wiki.util.SnowFlake;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
 
     @GetMapping("/list")
@@ -51,6 +66,13 @@ public class UserController {
         userReq.setPassword(DigestUtils.md5DigestAsHex(userReq.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp=userService.login(userReq);
+
+        //生成单点登录token，并放入redis中
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{}，并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),3600*24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
--- src/main/java/com/Knowledge/wiki/resp/UserLoginResp.java
+++ src/main/java/com/Knowledge/wiki/resp/UserLoginResp.java
@@ -7,6 +7,15 @@ public class UserLoginResp {
 
     private String name;
 
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     public Long getId() {
         return id;
@@ -34,14 +43,12 @@ public class UserLoginResp {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
     }
+
 }
